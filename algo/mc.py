b5bd236e4068026b70e9d4928fd6f0cc6ecf5cb4
Ryan Tang <ztang484@gmail.com>
2021-12-04 15:46:04 -0500

    minors

--- algo/mc.py
+++ algo/mc.py
@@ -28,6 +28,16 @@ class MCIteration(GPI):
     gamma: float, the reward discount rate
     alpha: float (default None), the learning rate
         - None, would use same average method instead 
+
+    Algorithm NOTE
+    -------------
+    Monte Carlo mimics brutal force approach in estimating the value function
+    it is really slow and error prone when getting bad samples.
+    And it will happen due to the inheritant random nature.
+    
+    For each policy π, it searchs through a combination of [T ^ (|S| x |A|)] possiabilities.
+    It is really large, and samples are sparse.
+    Therefore, always to use leinent batch size while using MC.
     """
     def fit(self, gamma=1, alpha=None):
         """Setting the algorithm"""
@@ -35,35 +45,33 @@ class MCIteration(GPI):
         self.alpha = alpha
         self.hist = []
 
-        # list used for targeted state-action policy improvment
-        self.last_updated_s = set()
-
         # state-action pair seen counts for step size inferring
         if self.alpha is None:
             self.sa_counts = {k:0 for k in product(self.env.S, self.env.A)}
 
-    def transform(self, iter=1000, kbatch=30, max_steps=None, pbar_leave=True):
+    def transform(self, iter=1000, kbatch=30, pbar_leave=True):
         """
         Obtain the optimal policy
 
         parameter
         ---------
         kbatch: int, the number of episode in a batch to process in each policy evaluation
-        max_steps: int (default None), max steps on taking a episode for early stopping if too long
         pbar_leave: bool, wheter to leave the progress bar on display
         """
         for _ in tqdm(range(iter), leave=pbar_leave):
-            self.evaluate_policy(kbatch, max_steps)
-            self.improve_policy()
+            eps = self.get_episodes(kbatch)
+            updated = self.evaluate_policy(eps)
+            self.improve_policy(updated)
+            self.hist.append(eps)
 
-    def evaluate_policy(self, kbatch=30, max_steps=None):
+
+    def evaluate_policy(self, eps):
         """Update q(a|s) with newly generated MC episode"""
         # construct returns for each (s,a) pair
-        epso_lst = self.get_episodes(n=kbatch, max_steps=max_steps)
         qs = {}
-        for epso in epso_lst:
+        for ep in eps:
             ret = 0
-            for step in epso[::-1]:  # start from the terminal step
+            for step in ep[::-1]:  # start from the terminal step
                 sa = step.s0, step.a0  # tuple pair
                 ret = self.gamma * ret + step.r1
                 if sa not in qs:
@@ -74,13 +82,12 @@ class MCIteration(GPI):
 
         # update q(a|s) with given new information
         # each (s,a) only updates once within a batch
+        updated = set()
         for sa, rets in qs.items():
             # step size α
             if not self.alpha:
-                step_size = self.sa_counts[sa]
-                step_size += 1
-                self.sa_counts[sa] = step_size
-                α = 1 / step_size
+                self.sa_counts[sa] += 1
+                α = 1 / self.sa_counts[sa]
             else:
                 α = self.alpha
             
@@ -92,28 +99,24 @@ class MCIteration(GPI):
             self.qvalue.set_q(s0, a0, new_q)
 
             # update seem states
-            self.last_updated_s.add(s0)
-        
-        self.hist.append(epso_lst)
+            updated.add(s0)
+        return updated
     
-    def improve_policy(self):
+    def improve_policy(self, updated):
         """
         Improve the existing policy by greedifying in respect to the
             current value estimates argmax[a] q(a|s)
             with ϵ-soft policy to ensure exploration in Monte Carlo
         """
-        for state in self.last_updated_s:
+        for state in updated:
             self.policy.greedify(state, self.qvalue[state])
         
-        self.last_updated_s.clear()
-
-    def get_episodes(self, n=1, max_steps=None):
+    def get_episodes(self, n=1):
         """Generate n episodes of data through Monte Carlo"""
         epso_lst = []
         for _ in range(n):
             epso = Episode()
             s0 = self.env.start()
-            i = 0
             while True:
                 a = np.random.choice(self.env.A, p=self.policy[s0])
                 s1, r = self.env.step(a)
@@ -121,10 +124,6 @@ class MCIteration(GPI):
                 epso.add_step(s0, a, r, s1, is_t)
                 s0 = s1
 
-                # fully stop or early stop
-                i += 1
-                if is_t or (max_steps is not None and i >= max_steps):
-                    break
-
+                if is_t: break
             epso_lst.append(epso)
         return epso_lst
--- tests/algo/test_mc.py
+++ tests/algo/test_mc.py
@@ -15,6 +15,15 @@ def mc1():
     mc_model = MCIteration(grid, qvalue, policy)
     return mc_model
 
+@pytest.fixture
+def mc2():
+    grid = GridWorld(2, 2)
+    qvalue = TabularQValue(grid.S, grid.A)
+    policy = TabularPolicy(grid.S, grid.A, epsilon=0.05)
+
+    mc_model = MCIteration(grid, qvalue, policy)
+    return mc_model
+
 
 def test_init(mc1):
     mc = mc1
@@ -23,7 +32,6 @@ def test_init(mc1):
     assert mc.gamma == 1
     assert mc.alpha == 0.1
     assert len(mc.hist) == 0
-    assert len(mc.last_updated_s) == 0
 
     mc.fit(gamma=1, alpha=None)
     assert 'sa_counts' in mc.__dict__
@@ -35,96 +43,46 @@ def test_get_episodes(mc1):
     assert len(eps) == 5
     assert type(eps[0]) is Episode
 
-    eps = mc.get_episodes(n=10, max_steps=1)
-    for ep in eps:
-        assert len(ep) <= 1
-
 
 def test_transform(mc1):
     mc = mc1
-    with patch.object(MCIteration, 'evaluate_policy'), \
+    mc.fit(gamma=1, alpha=None)
+    with patch.object(MCIteration, 'get_episodes', return_value=0), \
+         patch.object(MCIteration, 'evaluate_policy', return_value=1), \
          patch.object(MCIteration, 'improve_policy'):
-        mc.transform(iter=10, kbatch=30, max_steps=None, pbar_leave=False)
-        mc.evaluate_policy.assert_called_with(30, None)
-        mc.improve_policy.assert_called()
+        mc.transform(iter=10, kbatch=30, pbar_leave=False)
+        mc.evaluate_policy.assert_called_with(0)
+        mc.improve_policy.assert_called_with(1)
+        assert 0 in mc.hist
 
 
-def test_iteration(mc1):
-    mc = mc1
+def test_iteration(mc2):
+    mc = mc2
     np.random.seed(45345)
     mc.fit(gamma=1, alpha=None)
-    mc.transform(500, kbatch=30, max_steps=None, pbar_leave=False)
+    mc.transform(500, kbatch=30, pbar_leave=False)
 
     values = np.round(mc.qvalue.get_all_values(mc.policy).reshape(mc.env.shape), 0)
-    np.allclose(
+    assert np.allclose(
         values,
         np.array([
-            [ 0, -1, -2, -3],
-            [-1, -2, -3, -2],
-            [-2, -3, -2, -1],
-            [-3, -2, -1,  0],
-        ])
-    )
-
-    np.allclose(
-        mc.policy.to_numpy(),
-        np.array([
-            [0.25  , 0.25  , 0.25  , 0.25  ],
-            [0.0125, 0.0125, 0.9625, 0.0125],
-            [0.0125, 0.0125, 0.9625, 0.0125],
-            [0.0125, 0.9625, 0.0125, 0.0125],
-            [0.9625, 0.0125, 0.0125, 0.0125],
-            [0.0125, 0.0125, 0.9625, 0.0125],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.9625, 0.0125, 0.0125],
-            [0.9625, 0.0125, 0.0125, 0.0125],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.9625, 0.0125, 0.0125],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.25  , 0.25  , 0.25  , 0.25  ],
+            [ 0, -1],
+            [-1,  0],
         ])
     )
 
 
-def test_iteration_fixed_alpha(mc1):
-    mc = mc1
+def test_iteration_fixed_alpha(mc2):
+    mc = mc2
     np.random.seed(45345)
     mc.fit(gamma=1, alpha=0.1)
-    mc.transform(500, kbatch=30, max_steps=None, pbar_leave=False)
+    mc.transform(500, kbatch=30, pbar_leave=False)
 
-    # we should converge to the same results within leinent precision
     values = np.round(mc.qvalue.get_all_values(mc.policy).reshape(mc.env.shape), 0)
-    np.allclose(
+    assert np.allclose(
         values,
         np.array([
-            [ 0, -1, -2, -3],
-            [-1, -2, -3, -2],
-            [-2, -3, -2, -1],
-            [-3, -2, -1,  0],
-        ])
-    )
-
-    np.allclose(
-        mc.policy.to_numpy(),
-        np.array([
-            [0.25  , 0.25  , 0.25  , 0.25  ],
-            [0.0125, 0.0125, 0.9625, 0.0125],
-            [0.0125, 0.0125, 0.9625, 0.0125],
-            [0.0125, 0.9625, 0.0125, 0.0125],
-            [0.9625, 0.0125, 0.0125, 0.0125],
-            [0.0125, 0.0125, 0.9625, 0.0125],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.9625, 0.0125, 0.0125],
-            [0.9625, 0.0125, 0.0125, 0.0125],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.9625, 0.0125, 0.0125],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.0125, 0.0125, 0.0125, 0.9625],
-            [0.25  , 0.25  , 0.25  , 0.25  ],
+            [ 0, -1],
+            [-1,  0],
         ])
     )
