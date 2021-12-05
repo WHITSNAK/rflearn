49e945fce865cdd274ee2a3a479af0697940c225
Ryan Tang <ztang484@gmail.com>
2021-12-04 22:14:29 -0500

    add td(0) algo

---
+++ algo/td.py
@@ -0,0 +1,63 @@
+import numpy as np
+from tqdm import tqdm
+from .base import GPI
+from .episode import EpisodeStep
+from itertools import product
+
+
+class Sarsa(GPI):
+    def fit(self, gamma=1, alpha=None):
+        """Setting the algorithm"""
+        self.gamma = gamma
+        self.alpha = alpha
+        self.hist = []
+
+        # state-action pair seen counts for step size inferring
+        if self.alpha is None:
+            self.sa_counts = {k:0 for k in product(self.env.S, self.env.A)}
+
+    def transform(self, iter=1000, pbar_leave=True):
+        pbar = tqdm(total=iter, leave=pbar_leave)
+        for step in self.get_steps(iter):
+            state = self.evaluate_policy(step)
+            self.improve_policy(state)
+            pbar.update(1)
+        pbar.close()
+    
+    def evaluate_policy(self, step):
+        s0, a, r, s1, is_t = step
+
+        # step size α
+        if not self.alpha:
+            self.sa_counts[(s0, a)] += 1
+            α = 1 / self.sa_counts[(s0, a)]
+        else:
+            α = self.alpha
+
+        q = self.qvalue.get_q(s0, a)
+        a1 = self.policy.get_action(s1)
+        target = r + self.gamma * self.qvalue.get_q(s1, a1)
+        error = target - q
+        new_q = q + α * error
+        self.qvalue.set_q(s0, a, new_q)
+        self.hist.append(error)
+        return s0
+    
+    def improve_policy(self, state):
+        self.policy.greedify(state, self.qvalue[state])
+    
+    def get_steps(self, steps=100):
+        """Generate step data from ENV"""
+        cnt = 0
+        s0 = self.env.start()
+        while cnt < steps:
+            a = self.policy.get_action(s0)
+            s1, r = self.env.step(a)
+            is_t = self.env.is_terminal()
+            step = EpisodeStep(s0, a, r, s1, is_t)
+            yield step
+
+            s0 = s1
+            cnt += 1
+            if is_t:
+                s0 = self.env.start()
